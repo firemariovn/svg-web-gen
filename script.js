70826538366b2b68c73c87180a5b24708e998a55
Duc Tran <firemariovn@gmail.com>
2024-08-06 19:48:51 +0700

    feat: apply color for svg elements and downloadable

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+// TODO: Change Text
+// TODO: Refactor
+
 document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
-document.getElementById('apply-color').addEventListener('click', applyColor);
+
+document.getElementById('color-picker').addEventListener('input', applyColor); // Optional
+document.getElementById('color-picker2').addEventListener('input', applyColor2); // Optional
+document.getElementById('color-picker3').addEventListener('input', applyColorText); // Optional
+
+var svgContainer = document.getElementById('svg-container');
+
+document.getElementById('download').addEventListener('click', () => {
+    const serializer = new XMLSerializer();
+    //const svgString = serializer.serializeToString(svgElement);
+    const svgString = serializer.serializeToString(svgContainer);
+    const blob = new Blob([svgString], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'custom-logo.svg';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+});
 
 function handleFileSelect(event) {
     const file = event.target.files[0];
     if (file && file.type === 'image/svg+xml') {
         const reader = new FileReader();
         reader.onload = function(e) {
-            const svgContainer = document.getElementById('svg-container');
+            svgContainer = document.getElementById('svg-container');
             svgContainer.innerHTML = e.target.result;
         };
         reader.readAsText(file);
@@ -17,11 +40,31 @@ function handleFileSelect(event) {
 
 function applyColor() {
     const color = document.getElementById('color-picker').value;
-    const svgContainer = document.getElementById('svg-container');
-    const svgElements = svgContainer.querySelectorAll('path, circle, rect, polygon, polyline, ellipse, line');
+    const svgElements = svgContainer.querySelectorAll('.first-color');
     
     svgElements.forEach(element => {
-        element.setAttribute('fill', color);
+        var currentStyle = `fill: ${color};`;
+        element.setAttribute('style', currentStyle);
     });
 }
 
+function applyColor2() {
+    const color = document.getElementById('color-picker2').value;
+    const svgElements = svgContainer.querySelectorAll('.second-color');
+    
+    svgElements.forEach(element => {
+        var currentStyle = `fill: ${color};`;
+        element.setAttribute('style', currentStyle);
+    });
+}
+
+function applyColorText() {
+    const color = document.getElementById('color-picker3').value;
+    //const svgElements = svgContainer.querySelectorAll('path, circle, rect, polygon, polyline, ellipse, line');
+    const svgElements = svgContainer.querySelectorAll('.cls-7-a04'); // TODO
+    
+    svgElements.forEach(element => {
+        var currentStyle = `fill: ${color};`;
+        element.setAttribute('style', currentStyle);
+    });
+}
